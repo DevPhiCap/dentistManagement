b96d0bef8bdfa9f10dc920077abed5ac7980f752
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2023-08-18 18:03:43 +0700

    update

--- javascript/patientDetails.js
+++ javascript/patientDetails.js
@@ -1,5 +1,23 @@
 function redirectToCustomerTable() {
-    window.location.href = '../customer/customerTable.php';
+    var currentURL = new URL(window.location.href);
+    var startYear = currentURL.searchParams.get('startyear');
+    var endYear = currentURL.searchParams.get('endyear');
+
+    // Update the URL with query parameters
+    var newURL = '/customerManagement/customer/customerTable.php';
+    if (startYear && endYear) {
+        newURL += '?startyear=' + startYear + '&endyear=' + endYear;
+    } else if (startYear) {
+        newURL += '?startyear=' + startYear;
+    } else if (endYear) {
+        newURL += '?endyear=' + endYear;
+    }
+
+    // Replace the current state with the updated URL
+    history.replaceState({}, '', newURL);
+
+    // Go back to the previous page
+    window.history.back();
 }
 
 function handleImageUpload(inputId) {
